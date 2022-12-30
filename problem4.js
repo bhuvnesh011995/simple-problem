67df826aa85467e98d63f78adee6e2dc360f7024
bhuvnesh kumar <bhuvnesh011995@gmail.com>
2022-12-30 17:14:08 +0530

    added question in solution

--- problem4.js
+++ problem4.js
@@ -1,4 +1,4 @@
-//leetcode solution of the question 26
+//leetcode solution of the question 26 - remove the duplicate element form a given array
 var removeDuplicates = function(nums) {
     for(i=0;i<nums.length;i++){
         if(nums[i]===nums[i+1]){
--- problem5.js
+++ problem5.js
@@ -1,4 +1,4 @@
-//leetcode solution of the qestion 27
+//leetcode solution of the qestion 27- remove the particular element form the given array
 var removeElement = function(nums, val) {
     for(i=0;i<nums.length;i++)
     if(nums[i]===val){
