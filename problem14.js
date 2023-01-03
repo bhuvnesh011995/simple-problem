e97a79acb2f80c7934418ab3a3ec5b211f4f2f46
bhuvnesh kumar <bhuvnesh011995@gmail.com>
2023-01-03 12:40:14 +0530

    3/1/23

---
+++ problem14.js
@@ -0,0 +1,10 @@
+// leetcode solution of question 217
+var containsDuplicate = function(nums) {
+    nums = nums.sort();
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===nums[i+1]){
+            return true;
+        }
+    }
+    return false;
+};
