c77652eee8eb46a80ecfa58edde75e8a69747355
bhuvnesh kumar <bhuvnesh011995@gmail.com>
2023-01-25 13:17:19 +0530

    19/1/23

--- problem34.js
+++ problem34.js
@@ -3,6 +3,7 @@ var findMedianSortedArrays = function(nums1, nums2) {
     nums1 = nums1.concat(nums2).sort(function(a, b) {
         return a - b;
       });
+    //   nums1 = sortList(nums1);
     if(nums1.length%2!=0){
         return nums1[(nums1.length-1)/2];
     }
@@ -11,4 +12,20 @@ var findMedianSortedArrays = function(nums1, nums2) {
         return (nums1[a]+nums1[a-1])/2
     }
 };
+// var sortList = function(head) {
+//     let a;
+//     let b = head.length;
+//     for(i=0;i<b;i++){
+//         let k= i;
+//         for(j=i-1;j>=0;j--){
+//             if(head[j]>head[k]){
+//                 a = head[j];
+//                 head[j]= head[k];
+//                 head[k]= a;
+//                 k--
+//             }
+//         }
+//     }
+//     return head;
+//   };
 console.log(findMedianSortedArrays([3],[-2,-1]));
